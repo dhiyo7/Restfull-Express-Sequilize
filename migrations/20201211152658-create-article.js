4cb9bdb662613073944af1aa86faa781c0112f5c
dhiyo7 <dhiyoreksa12@gmail.com>
2020-12-12 04:48:45 +0700

    4 - fix timestamp

--- migrations/20201211152658-create-article.js
+++ migrations/20201211152658-create-article.js
@@ -30,7 +30,7 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
     });
   },
